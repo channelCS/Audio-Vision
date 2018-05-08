dc9a88f09947962781eccdd22c7b4d179a1010d4
Akshita Gupta <akshitadvlp@gmail.com>
2018-05-08 17:46:06 +0530

    Adding dnn py file

---
+++ DNN/mainfile.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 08 17:13:52 2018
+
+@author: adityac8
+"""
+
+# Suppress warnings
+import warnings
+warnings.simplefilter("ignore")
+
+# Clone the keras_aud library and place the path in ka_path variable
+import sys
+ka_path="e:/akshita_workspace/cc"
+sys.path.insert(0, ka_path)
+from keras_aud import aud_audio, aud_feature
+from keras_aud import aud_model, aud_utils
+
+# Make imports
+import csv
+import cPickle
+import numpy as np
+import scipy
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import KFold
+from keras.utils import to_categorical
+
+# This is where all audio files reside and features will be extracted
+audio_ftr_path='E:/akshita_workspace/'
+
+# We now tell the paths for audio, features and texts.
+wav_dev_fd   = audio_ftr_path+'dcase_data/audio/dev'
+wav_eva_fd   = audio_ftr_path+'dcase_data/audio/eva'
+dev_fd       = audio_ftr_path+'dcase_data/features/dev'
+eva_fd       = audio_ftr_path+'dcase_data/features/eva'
+label_csv    = '../utils/dcase_data/texts/dev/meta.txt'
+txt_eva_path = '../utils/dcase_data/texts/eva/test.txt'
+new_p        = '../utils/dcase_data/texts/eva/evaluate.txt'
+
+labels = [ 'bus', 'cafe/restaurant', 'car', 'city_center', 'forest_path', 'grocery_store', 'home', 'beach', 
+            'library', 'metro_station', 'office', 'residential_area', 'train', 'tram', 'park' ]
+
+# We define all model parameters here.
+prep='eval'               # dev or eval
+folds=4                   # Number of folds
+save_model=False          # True if we want to save model
+model_type='Functional'   # Can be Dynamic or Functional
+model='CNN'               # Name of model
+feature="logmel"          # Name of feature
+
+dropout1=0.1              # 1st Dropout
+act1='relu'               # 1st Activation
+act2='relu'               # 2nd Activation
+act3='softmax'            # 3rd Activation
+
+input_neurons=400         # Number of Neurons
+epochs=100                # Number of Epochs
+batchsize=128             # Batch Size
+num_classes=15            # Number of classes
+filter_length=3           # Size of Filter
+nb_filter=100             # Number of Filters
+
+agg_num=10                # Number of frames
+hop=10                    # Hop Length
+
+#We extract audio features
+aud_audio.extract(feature, wav_dev_fd, dev_fd+'/'+feature,'example.yaml')
+aud_audio.extract(feature, wav_eva_fd, eva_fd+'/'+feature,'example.yaml')
+
+def GetAllData(fe_fd, csv_file):
+    """
+    Loads all the features saved as pickle files.
+    Input: Features folder(str), CSV file(str)
+    Output: Loaded features(np array) and labels(np array).
+    """
+    # read csv
+    with open( csv_file, 'rb') as f:
+        reader = csv.reader(f)
+        lis = list(reader)
+    
+    # init list
+    X3d_all = []
+    y_all = []
+    i=0
+    for li in lis:
+        # load data
+        [na, lb] = li[0].split('\t')
+        na = na.split('/')[1][0:-4]
+        path = fe_fd + '/' + na + '.f'
+        try:
+            X = cPickle.load( open( path, 'rb' ) )
+        except Exception as e:
+            print 'Error while parsing',path
+            continue
+        # reshape data to (n_block, n_time, n_freq)
+        i+=1
+        X3d = aud_utils.mat_2d_to_3d( X, agg_num, hop )
+        X3d_all.append( X3d )
+        y_all += [ aud_utils.lb_to_id[lb] ] * len( X3d )
+    
+    print "Features loaded",i                
+    print 'All files loaded successfully'
+    # concatenate list to array
+    X3d_all = np.concatenate( X3d_all )
+    y_all = np.array( y_all )
+    
+    return X3d_all, y_all
+
+def test(md,csv_file):
+    # load name of wavs to be classified
+    with open( csv_file, 'rb') as f:
+        reader = csv.reader(f)
+        lis = list(reader)
+    
+    # do classification for each file
+    names = []
+    pred_lbs = []
+    
+    for li in lis:
+        names.append( li[0] )
+        na = li[0][6:-4]
+        #audio evaluation name
+        fe_path = eva_fd + '/' + na + '.f'
+        X0 = cPickle.load( open( fe_path, 'rb' ) )
+        X0 = aud_utils.mat_2d_to_3d( X0, agg_num, hop )
+        
+        X0 = aud_utils.mat_3d_to_nd(model,X0)
+    
+        # predict
+        p_y_preds = md.predict(X0)        # probability, size: (n_block,label)
+        preds = np.argmax( p_y_preds, axis=-1 )     # size: (n_block)
+        b = scipy.stats.mode(preds)
+        pred = int( b[0] )
+        pred_lbs.append( aud_utils.id_to_lb[ pred ] )
+    
+    pred = []    
+    # write out result
+    for i1 in xrange( len( names ) ):
+        fname = names[i1] + '\t' + pred_lbs[i1] + '\n' 
+        pred.append(fname)
+        
+    print 'write out finished!'
+    truth = open(new_p,'r').readlines()
+    pred = [i.split('\t')[1].split('\n')[0]for i in pred]
+    truth = [i.split('\t')[1]for i in truth]
+    pred.sort()
+    truth.sort()
+    return truth,pred
+
+tr_X, tr_y = GetAllData( dev_fd+'/'+feature, label_csv)
+
+print(tr_X.shape)
+print(tr_y.shape)    
+    
+tr_X=aud_utils.mat_3d_to_nd(model,tr_X)
+print(tr_X.shape)
+dimx=tr_X.shape[-2]
+dimy=tr_X.shape[-1]
+
+if prep=='dev':
+    cross_validation=True
+else:
+    cross_validation=False
+    
+miz=aud_model.Functional_Model(input_neurons=input_neurons,cross_validation=cross_validation,dropout1=dropout1,
+    act1=act1,act2=act2,act3=act3,nb_filter = nb_filter, filter_length=filter_length,
+    num_classes=num_classes,
+    model=model,dimx=dimx,dimy=dimy)
+
+np.random.seed(68)
+if cross_validation:
+    kf = KFold(len(tr_X),folds,shuffle=True,random_state=42)
+    results=[]    
+    for train_indices, test_indices in kf:
+        train_x = [tr_X[ii] for ii in train_indices]
+        train_y = [tr_y[ii] for ii in train_indices]
+        test_x  = [tr_X[ii] for ii in test_indices]
+        test_y  = [tr_y[ii] for ii in test_indices]
+        train_y = to_categorical(train_y,num_classes=len(labels))
+        test_y = to_categorical(test_y,num_classes=len(labels)) 
+        
+        train_x=np.array(train_x)
+        train_y=np.array(train_y)
+        test_x=np.array(test_x)
+        test_y=np.array(test_y)
+        print "Development Mode"
+
+        #get compiled model
+        lrmodel=miz.prepare_model()
+
+        if lrmodel is None:
+            print "If you have used Dynamic Model, make sure you pass correct parameters"
+            raise SystemExit
+        #fit the model
+        lrmodel.fit(train_x,train_y,batch_size=batchsize,epochs=epochs,verbose=1)
+        
+        #make prediction
+        pred=lrmodel.predict(test_x, batch_size=32)
+
+        pred = [ii.argmax()for ii in pred]
+        test_y = [ii.argmax()for ii in test_y]
+
+        results.append(accuracy_score(pred,test_y))
+        print accuracy_score(pred,test_y)
+        jj=str(set(list(test_y)))
+        print "Unique in test_y",jj
+    print "Results: " + str( np.array(results).mean() )
+else:
+    train_x=np.array(tr_X)
+    train_y=np.array(tr_y)
+    print "Evaluation mode"
+    lrmodel=miz.prepare_model()
+    train_y = to_categorical(train_y,num_classes=len(labels))
+        
+    #fit the model
+    lrmodel.fit(train_x,train_y,batch_size=batchsize,epochs=epochs,verbose=1)
+    
+    truth,pred=test(lrmodel,txt_eva_path,new_p,model)
+
+    acc=aud_utils.calculate_accuracy(truth,pred)
+    print "Accuracy %.2f prcnt"%acc
--- README.md
+++ README.md
@@ -5,21 +5,21 @@
 + Implementation
 ```
 
-<h3> Implementation and reviews of Audio & Computer vision related papers in `python`. </h3>
+### Implementation and reviews of Audio & Computer vision related papers in `python`.
 
-[1] Deep Neural Network Baseline For Dcase Challenge 2016 [[Paper](http://www.cs.tut.fi/sgn/arg/dcase2016/documents/challenge_technical_reports/DCASE2016_Kong_3008.pdf)] [[Implementation](https://github.com/channelCS/Summaries/tree/master/Audio_Dcase_DNN_Baseline)] 
+[1] Deep Neural Network Baseline For Dcase Challenge 2016 [[Paper](http://www.cs.tut.fi/sgn/arg/dcase2016/documents/challenge_technical_reports/DCASE2016_Kong_3008.pdf)] [[Implementation](https://github.com/channelCS/Summaries/tree/master/DNN)] 
 
-[2] CQT-Based Convolutional Neural Networks for Audio Scene Classification and Domestic Audio Tagging [[Paper](http://www.cs.tut.fi/sgn/arg/dcase2016/documents/challenge_technical_reports/DCASE2016_Lidy_4007.pdf)] [[Implementation](https://github.com/akshitac8/Summaries/tree/master/Audio_Dcase_CNN_CQT)]
+[2] CQT-Based Convolutional Neural Networks for Audio Scene Classification and Domestic Audio Tagging [[Paper](http://www.cs.tut.fi/sgn/arg/dcase2016/documents/challenge_technical_reports/DCASE2016_Lidy_4007.pdf)] [[Implementation](https://github.com/akshitac8/Summaries/tree/master/Parallel_CNN)]
 
-[3] A convolutional neural network approach for acoustic scene classification [[Paper](https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7966035)] [[Implementation](https://github.com/akshitac8/Summaries/tree/master/Audio_Dcase_CNN_logmel)] 
+[3] A convolutional neural network approach for acoustic scene classification [[Paper](https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7966035)] [[Implementation](https://github.com/akshitac8/Summaries/tree/master/Deep_CNN)] 
 
 [4] Convolutional Recurrent Neural Networks for Polyphonic Sound Event Detection [[Paper](https://arxiv.org/pdf/1702.06286.pdf)] [[Implementation](https://github.com/akshitac8/Summaries/tree/master/CRNN)] 
 
-[5] FrameCNN: A Weakly-Supervised Learning Framework for Frame-Wise Acoustic Event Detection and Classification [[Paper](https://www.cs.tut.fi/sgn/arg/dcase2017/documents/challenge_technical_reports/DCASE2017_Chou_102.pdf)][[Implementation](https://github.com/akshitac8/Summaries/tree/master/FrameCNN)]
+[5] FrameCNN: A Weakly-Supervised Learning Framework for Frame-Wise Acoustic Event Detection and Classification [[Paper](https://www.cs.tut.fi/sgn/arg/dcase2017/documents/challenge_technical_reports/DCASE2017_Chou_102.pdf)][[Implementation](https://github.com/akshitac8/Summaries/tree/master/Frame_CNN)]
 
 [6] Sequence to Sequence Autoencoders for Unsupervised Representation Learning From Audio [[Paper](https://www.cs.tut.fi/sgn/arg/dcase2017/documents/workshop_papers/DCASE2017Workshop_Amiriparian_172.pdf)][[Implementation](https://github.com/akshitac8/Summaries/tree/master/seq2seq_RNN)]
 
-[7] Attention and Localization based on a Deep Convolutional Recurrent Model for Weakly Supervised Audio Tagging [[Paper](https://arxiv.org/pdf/1703.06052.pdf)][[Implementation](https://github.com/akshitac8/Summaries/tree/master/ATT-CGRNN)] 
+[7] Attention and Localization based on a Deep Convolutional Recurrent Model for Weakly Supervised Audio Tagging [[Paper](https://arxiv.org/pdf/1703.06052.pdf)][[Implementation](https://github.com/akshitac8/Summaries/tree/master/Attention_CGRNN)] 
 
 [8] Exploring Models and Data for Image Question Answering [[paper](https://arxiv.org/pdf/1505.02074.pdf)][Implementation]
 
@@ -27,17 +27,17 @@
 ```diff
 - Application
 ```
-<h3> Application Deployed using heroku and Flask with python and JS </h3>
+### Application Deployed using heroku and Flask with python and JS
 
 [1] Digit classifier [Paper] [implementation] [[Application](https://github.com/channelCS/digit-identify)] 
 
 [2] MNIST Random Digit Regenerator [Paper] [Implementation] [[Application](https://github.com/channelCS/image-generate)]
 
-<h3> FeedBack </h3>
+### FeedBack
 
 All kind of feedback (code style, bugs, comments etc.) is welcome. Please open an issue on this Reposistory.
 
-<h3>Team Roles</h3>
+### Team Roles
 
 Aditya Arora  :
 
